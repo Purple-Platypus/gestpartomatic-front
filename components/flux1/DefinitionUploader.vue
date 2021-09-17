f8b03875afba3b4f6914ac5fe79a7ca5302acda4
Yann-Bernardon <yann.bernardon@dgfip.finances.gouv.fr>
2021-09-17 21:30:44 +0200

    Optimisation du parser CSV du fluxomatic

--- components/flux1/DefinitionUploader.vue
+++ components/flux1/DefinitionUploader.vue
@@ -12,6 +12,7 @@
 </template>
 
 <script>
+import Papa from 'papaparse';
 import Uploader from '../commons/Uploader.vue';
 
 export default {
@@ -19,102 +20,68 @@ export default {
     props: ['color', 'depressed', 'outlined', 'small'],
     data() {
         return {
-            titles: [
-                'position',
-                'nature',
-                'required',
-                'label',
-                'description',
-                'a',
-                'c',
-                'db_column',
-                'comment'
-            ]
+            titlesMap: {
+                'Pos.': 'position',
+                Nature: 'nature',
+                'O/F': 'required',
+                Libellé: 'label',
+                Observation: 'description',
+                A: 'a',
+                C: 'c',
+                'Donnée en base': 'db_column',
+                Commentaire: 'comment'
+            }
         };
     },
     methods: {
         handleUpload(fileContent) {
-            this.parseCSV(fileContent);
-        },
-
-        parseCSV(csv) {
-            const definitionsArray = this.csv2Array(csv);
-            definitionsArray.shift();
-
-            const trimedDefinitionsArray = definitionsArray.filter(
-                property => property.length > 1
-            );
-
-            const definitionsObject = trimedDefinitionsArray.map(item => {
-                return this.csvLine2Object(item);
-            });
-
-            this.$emit('input', definitionsObject);
-        },
-
-        csv2Array(csv) {
-            var pattern = new RegExp(
-                '(\\;|\\r?\\n|\\r|^)(?:"([^"]*(?:""[^"]*)*)"|([^"\\;\\r\\n]*))',
-                'gi'
-            );
-
-            var rows = [[]];
-            var matches = false;
-            while ((matches = pattern.exec(csv))) {
-                var matched_delimiter = matches[1];
-                if (matched_delimiter.length && matched_delimiter !== ';') {
-                    rows.push([]);
-                }
-                var matched_value;
-                if (matches[2]) {
-                    matched_value = matches[2].replace(
-                        new RegExp('""', 'g'),
-                        '"'
-                    );
-                } else {
-                    matched_value = matches[3];
-                }
-                rows[rows.length - 1].push(matched_value);
-            }
-            return rows;
-        },
-
-        csvLine2Object(csvLine) {
-            const csvLineAsObject = {};
-            csvLine.forEach((propertyValue, index) => {
-                const key = this.titles[index];
-
-                switch (key) {
-                    case 'position':
-                        csvLineAsObject[key] = ~~propertyValue;
-                        break;
-                    case 'nature':
+            Papa.parse(fileContent, {
+                skipEmptyLines: true,
+                escapeChar: '"',
+                header: true,
+                transformHeader: this.formatTitle,
+                transform: this.formatData,
+                complete: res => {
+                    res.data.map(def => {
+                        console.log(def);
                         const type =
-                            propertyValue.substring(0, 1) == 'X'
+                            def.nature.substring(0, 1) == 'X'
                                 ? 'STRING'
                                 : 'INT';
-                        const length = propertyValue.split(/(\(|\))/);
-                        csvLineAsObject.nature = type;
-                        csvLineAsObject.length = ~~length[2];
-                        break;
-                    case 'required':
-                        csvLineAsObject[key] = propertyValue == 'O';
-                        break;
-                    case 'a':
-                        csvLineAsObject[key] = propertyValue == 'A';
-                        break;
-                    case 'c':
-                        csvLineAsObject[key] = propertyValue == 'C';
-                        break;
-                    case 'label':
-                    case 'description':
-                    case 'db_column':
-                    case 'comment':
-                        csvLineAsObject[key] = propertyValue;
-                        break;
+                        const length = def.nature.split(/(\(|\))/);
+
+                        def.nature = type;
+                        def.length = ~~length[2];
+                    });
+
+                    this.$emit('input', res.data);
                 }
             });
-            return csvLineAsObject;
+        },
+        formatTitle(title) {
+            return this.titlesMap[title];
+        },
+        formatData(data, key) {
+            let formattedValue;
+            switch (key) {
+                case 'position':
+                    formattedValue = ~~data;
+                    break;
+                case 'required':
+                    formattedValue = data == 'O';
+                    break;
+                case 'a':
+                    formattedValue = data == 'A';
+                    break;
+                case 'c':
+                    formattedValue = data == 'C';
+                    break;
+                default:
+                    formattedValue = data.trim();
+                    break;
+            }
+
+            return formattedValue;
         }
     }
 };
